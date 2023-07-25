2ddb8df14a084aa1b98f2538e27827d1db336aa6
Josiah <josiahufono@gmail.com>
2023-07-25 09:07:20 -0700

    specifier_checker - checks if character after % is a valid specifier and prints the specified argument. print_char - prints a charatcer to stdoutput

--- main.h
+++ main.h
@@ -4,6 +4,23 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * struct functn - struct for the printer's specifier
+ * @functionptr: function for printing
+ * @t: char to be checked
+*/
+
 int _printf(const char *format, ...);
+int (*specifier_checker(const char *format))(va_list);
+
+typedef struct functn
+{
+	char *t;
+	int (*functionptr)(va_list);
+} functn_t;
+
+int print_char(va_list);
+int print_str(va_list);
+int print_pcent(va_list);
 
 #endif
